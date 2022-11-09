1e5beae5abfe66fbd6e9a8a77d1451f88aa1e39c
Jiahe Chu <jiahe.chu@tum.de>
2022-11-09 21:13:08 +0100

    finished template input file, added some code

--- src/executor.py
+++ src/executor.py
@@ -1,7 +1,13 @@
 """
 Execute the toolbox.
 """
+from frontend import read_input
 
 
 def executor(input_setup, output_setup):
+    if input_setup['used_pre_defined_net'] is '':
+        net = read_input.read_input(input_setup['input_path'])
+    else:
+        pass
+
     return 0
