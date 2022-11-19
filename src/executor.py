2688f415ea0505bde3bd2c069a1bb4451af692d4
Jiahe Chu <jiahe.chu@tum.de>
2022-11-19 12:13:34 +0100

    init scenarios structure

--- src/executor.py
+++ src/executor.py
@@ -1,18 +1,19 @@
 """
 Execute the toolbox.
 """
-import pandapower as pp
 from frontend import read_input
+from scenarios import apply_scenario
 
 
 def executor(input_setup, output_setup):
     # create pandapower network from user-defined Excel file or pre-defined topology
     if input_setup['used_pre_defined_net'] is '':
-        net = read_input.read_input(input_setup['loadgen_path'], input_setup['topology_path'], input_setup['output_path']+'/network.xlsx')
+        net = read_input.read_input(input_setup['loadgen_path'], input_setup['topology_path'], output_setup['output_path'])
     else:
         pass
 
     # apply scenario from data
-
+    if input_setup['used_scenario'] != '':
+        net = apply_scenario.apply_scenario(net, input_setup['used_scenario'], **input_setup['scenario_para'])
 
     return 0
