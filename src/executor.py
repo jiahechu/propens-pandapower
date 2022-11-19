46f3fb8f3191fed85fb7d8df1776b6ce3e05b8e8
Jiahe Chu <jiahe.chu@tum.de>
2022-11-19 11:35:06 +0100

    fixedbugs

--- src/executor.py
+++ src/executor.py
@@ -8,7 +8,7 @@ from frontend import read_input
 def executor(input_setup, output_setup):
     # create pandapower network from user-defined Excel file or pre-defined topology
     if input_setup['used_pre_defined_net'] is '':
-        net = read_input.read_input(input_setup['loadgen_path'], input_setup['topology_path'], input_setup['output_path'])
+        net = read_input.read_input(input_setup['loadgen_path'], input_setup['topology_path'], input_setup['output_path']+'/network.xlsx')
     else:
         pass
 
