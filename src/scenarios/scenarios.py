b65b95d6fc82622aa6de6dc8c630027e478441ab
Jiahe Chu <jiahe.chu@tum.de>
2022-11-30 23:37:49 +0100

    added kerber network example, added time series information, adjusted code structure, fixed bug on template files, now everything is working

--- src/scenarios/scenarios.py
+++ src/scenarios/scenarios.py
@@ -18,6 +18,11 @@ def scenario_pv_gen(net, para):
     if para < 0 or para > 2:
         raise ValueError('The parameter for pre-defined pv-gen scenario should between 0 and 2 (0% to 200%)')
     else:
+        # static generator
+        net.sgen['p_mw'][net.sgen['type'] == 'pv'] *= para
+        net.sgen['q_mvar'][net.sgen['type'] == 'pv'] *= para
+
+        # generator
         net.gen['p_mw'][net.gen['type'] == 'pv'] *= para
 
     return net
@@ -38,6 +43,11 @@ def scenario_wind_gen(net, para):
     if para < 0 or para > 2:
         raise ValueError('The parameter for pre-defined wind-gen scenario should between 0 and 2 (0% to 200%)')
     else:
+        # static generator
+        net.sgen['p_mw'][net.sgen['type'] == 'wind'] *= para
+        net.sgen['q_mvar'][net.sgen['type'] == 'wind'] *= para
+
+        # generator
         net.gen['p_mw'][net.gen['type'] == 'wind'] *= para
 
     return net
@@ -59,6 +69,7 @@ def scenario_conventional_pp_gen(net, para):
         raise ValueError('The parameter for pre-defined conv-pp scenario should between 0 and 2 (0% to 200%)')
     else:
         net.sgen['p_mw'][net.sgen['type'] == 'conv pp'] *= para
+        net.sgen['q_mvar'][net.sgen['type'] == 'conv pp'] *= para
 
     return net
 
@@ -78,6 +89,7 @@ def scenario_load(net, para):
         raise ValueError('The parameter for pre-defined load scenario should between 0 and 2 (0% to 200%)')
     else:
         net.load['p_mw'][:] *= para
+        net.load['q_mvar'][:] *= para
     
     return net
 
