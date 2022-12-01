5c289a8d7b7d834fba020e39c887f52fee3561ae
Jiahe Chu <jiahe.chu@tum.de>
2022-12-01 16:11:25 +0100

    added ts function, fixed bugs

--- src/scenarios/scenarios.py
+++ src/scenarios/scenarios.py
@@ -24,7 +24,6 @@ def scenario_pv_gen(net, para):
 
         # generator
         net.gen['p_mw'][net.gen['type'] == 'pv'] *= para
-        net.gen['q_mvar'][net.gen['type'] == 'PV'] *= para
 
     return net
 
@@ -50,7 +49,6 @@ def scenario_wind_gen(net, para):
 
         # generator
         net.gen['p_mw'][net.gen['type'] == 'wind'] *= para
-        net.gen['q_mvar'][net.gen['type'] == 'wind'] *= para
 
     return net
 
@@ -88,7 +86,7 @@ def scenario_load(net, para):
         net: pandapower network after applying the scenario.
     """
     if para < 0:
-        raise ValueError('The parameter for pre-defined load scenario should bigger than 0 (>0%)')
+        raise ValueError('The parameter for pre-defined load scenario should be bigger than 0 (>0%)')
     else:
         net.load['p_mw'][:] *= para
         net.load['q_mvar'][:] *= para
@@ -97,44 +95,50 @@ def scenario_load(net, para):
 
 
 def scenario_trafo_cap(net, para):
-    
+    """
+    Change all transformer capacities to given percent.
+
+    Args:
+        net: pandapower network.
+        para: percent of changing (0-2).
+
+    Returns:
+        net: pandapower network after applying the scenario.
+    """
     if para < 0.5 or para > 2:
-        raise ValueError('The parameter for pre-defined trafo scenario should between 0,5 and 2 (50% to 200%)')
-    else
+        raise ValueError('The parameter for pre-defined trafo capacity scenario should between 0,5 and 2 (50% to 200%)')
+    else:
         net.trafo['sn_mva'][:] *= para
+
     return net
 
 
 def scenario_lines_cap(net, para):
-    
     if para < 0:
-        raise ValueError('The parameter for pre-defined lines_cap scenario should bebigger than 0 (>0%)')
-    elif para > 1.95;
+        raise ValueError('The parameter for pre-defined lines_cap scenario should be bigger than 0 (>0%)')
+    elif para > 1.95:
         print("Warning! Overload may happen! Now adding parallel lines instead of changing the thermal current ")
-        net.line['parallel'][:] *= round(para,0)
-    else
+        net.line['parallel'][:] *= round(para, 0)
+    else:
         print("Input value has impact only during optimal power flow analysis")
         net.line['max_i_ka'][:] *= para
+
     return net
 
 
 def scenario_storage(net, para):
-
-    if para < 0 or para > 1:
-        raise ValueError('The parameter for pre-defined storage scenario should between 0 and 1 (0% to 100%)')
-    else
-        net.storage['max_e_mwh'][:] *= para
+    # if para < 0 or para > 1:
+    #     raise ValueError('The parameter for pre-defined storage scenario should between 0 and 1 (0% to 100%)')
+    # else
+    #     net.storage['max_e_mwh'][:] *= para
     return net
 
 
 def scenario_switch(net, zone):
-   
-    if para = 0;
-        net.switch['closed'][:] = False
-
-    elif para =1;
-        net.switch['closed'][:] = True
-
-    else
-        raise ValueError('The parameter for pre-defined switch scenario should be 0 or 1')
+    # if para == 0:
+    #     net.switch['closed'][:] = False
+    # elif para == 1:
+    #     net.switch['closed'][:] = True
+    # else:
+    #     raise ValueError('The parameter for pre-defined switch scenario should be 0 or 1')
     return net
--- start.py
+++ start.py
@@ -6,7 +6,7 @@ from src import executor
 
 # summarize all input setup in a dictionary
 # available pre-defined scenarios: pv_gen, wind_gen, conventional_pp_gen, load, trafo_cap, line_cap, storage, switch
-input_setup = {
+input_setup = {     # (scenario path, name of used pre-defined scenario, ggf. pre-defined scenario parameter)
     'topology_path': './example/kerber_landnetz_fl2/topology.xlsx',
     'scenario_setup': [('./example/kerber_landnetz_fl2/scenarios/basic.xlsx', 'load', 0.9),
                        ('./example/kerber_landnetz_fl2/scenarios/pv.xlsx', 'pv_gen', 1.5),
